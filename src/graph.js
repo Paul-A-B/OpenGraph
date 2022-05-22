634ee5f17b7d33dfc291cead6942b8d7e82e5e52
Paul-A-B <104840626+Paul-A-B@users.noreply.github.com>
2022-05-22 21:37:03 +0200

    2D geht

--- src/graph.js
+++ src/graph.js
@@ -8,47 +8,49 @@ renderer.setSize(window.innerWidth, window.innerHeight, false);
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
-  1,
-  500
+  0.01,
+  9999
 );
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 25);
 camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 
-const planeGeometry = new THREE.PlaneGeometry(100, 100);
-const planeMaterial = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  side: THREE.DoubleSide,
-});
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-scene.add(plane);
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableRotate = false;
+controls.maxDistance = 200;
+controls.minDistance = 0.1;
 
-for (let i = -5; i <= 5; i++) {
-  const verticalPoints = [];
-  verticalPoints.push(new THREE.Vector3(i * 1, -100, 0));
-  verticalPoints.push(new THREE.Vector3(i * 1, 100, 0));
-  const horizontalPoints = [];
-  horizontalPoints.push(new THREE.Vector3(-100, i * 1, 0));
-  horizontalPoints.push(new THREE.Vector3(100, i * 1, 0));
-
-  const verticalLineGeometry = new THREE.BufferGeometry().setFromPoints(
-    verticalPoints
-  );
-  const horizontalLineGeometry = new THREE.BufferGeometry().setFromPoints(
-    horizontalPoints
-  );
-
-  const verticalLine = new THREE.Line(verticalLineGeometry, lineMaterial);
-  const horizontalLine = new THREE.Line(horizontalLineGeometry, lineMaterial);
-
-  scene.add(verticalLine);
-  scene.add(horizontalLine);
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
-renderer.render(scene, camera);
+animate();
+
+// https://discourse.threejs.org/t/functions-to-calculate-the-visible-width-height-at-a-given-z-depth-from-a-perspective-camera/269
+const visibleHeightAtZDepth = (depth, camera) => {
+  // compensate for cameras not positioned at z=0
+  const cameraOffset = camera.position.z;
+  if (depth < cameraOffset) depth -= cameraOffset;
+  else depth += cameraOffset;
+
+  // vertical fov in radians
+  const vFOV = (camera.fov * Math.PI) / 180;
+
+  // Math.abs to ensure the result is always positive
+  return 2 * Math.tan(vFOV / 2) * Math.abs(depth);
+};
+
+const visibleWidthAtZDepth = (depth, camera) => {
+  const height = visibleHeightAtZDepth(depth, camera);
+  return height * camera.aspect;
+};
+
+let xScale, yScale;
 
 window.addEventListener("resize", reset);
 window.addEventListener("load", reset);
@@ -61,22 +63,64 @@ function reset() {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
   }
-  renderer.render(scene, camera);
+  xScale = visibleWidthAtZDepth(camera.position.z, camera) / 2; // doppelt so viel, wie man sehen kann
+  yScale = visibleHeightAtZDepth(camera.position.z, camera) / 2;
+  drawGrid();
 }
 
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+let activeGrid;
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableRotate = false;
-controls.update();
+function drawGrid() {
+  if (activeGrid) scene.remove(activeGrid);
 
-function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  renderer.render(scene, camera);
+  const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+
+  const grid = new THREE.Group();
+  const verticalLines = new THREE.Group();
+  const horizontalLines = new THREE.Group();
+
+  for (
+    let x = Math.round(-xScale + camera.position.x);
+    x <= Math.round(xScale + camera.position.x);
+    x++
+  ) {
+    const verticalPoints = [];
+    verticalPoints.push(new THREE.Vector3(x, -yScale + camera.position.y, 0));
+    verticalPoints.push(new THREE.Vector3(x, yScale + camera.position.y, 0));
+
+    const verticalLineGeometry = new THREE.BufferGeometry().setFromPoints(
+      verticalPoints
+    );
+
+    const verticalLine = new THREE.Line(verticalLineGeometry, lineMaterial);
+
+    verticalLines.add(verticalLine);
+  }
+  grid.add(verticalLines);
+
+  for (
+    let y = Math.round(-yScale + camera.position.y);
+    y <= Math.round(yScale + camera.position.y);
+    y++
+  ) {
+    const horizontalPoints = [];
+    horizontalPoints.push(new THREE.Vector3(-xScale + camera.position.x, y, 0));
+    horizontalPoints.push(new THREE.Vector3(xScale + camera.position.x, y, 0));
+
+    const horizontalLineGeometry = new THREE.BufferGeometry().setFromPoints(
+      horizontalPoints
+    );
+
+    const horizontalLine = new THREE.Line(horizontalLineGeometry, lineMaterial);
+
+    horizontalLines.add(horizontalLine);
+  }
+  grid.add(horizontalLines);
+
+  activeGrid = grid;
+
+  scene.add(grid);
 }
-
-animate();
 
 import { create, all } from "mathjs";
 
@@ -91,7 +135,11 @@ const outputArea = document.getElementById("output");
 
 inputArea.addEventListener("input", takeInput);
 
+const activeGraphs = [];
+const activeFunctions = [];
+
 function takeInput() {
+  activeFunctions.shift();
   let input = inputArea.value;
   try {
     if (input) {
@@ -101,31 +149,10 @@ function takeInput() {
         outputArea.textContent = `${input} = ${parser.evaluate(input)}`;
       }
 
-      const graphPoints = [];
+      activeFunctions.push(input);
 
-      for (let x = -5; x <= 5; x += 1 / renderer.domElement.width) {
-        parser.set("x", x);
-        const point = { x: x, y: parser.evaluate(input), z: 0 };
-        if (point.y)
-          graphPoints.push(new THREE.Vector3(point.x, point.y, point.z));
-      }
-
-      const graphGeometry = new THREE.TubeGeometry(
-        new THREE.CatmullRomCurve3(graphPoints),
-        graphPoints.length,
-        0.025,
-        8,
-        false
-      );
-
-      const graphMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-
-      const graph = new THREE.Mesh(graphGeometry, graphMaterial);
-
-      scene.add(graph);
-      renderer.render(scene, camera);
+      plotGraph(activeFunctions[0]);
     } else {
-      renderer.render(scene, camera);
       outputArea.textContent = "";
     }
   } catch (e) {
@@ -135,4 +162,48 @@ function takeInput() {
       outputArea.textContent = `${e.message}`;
     }
   }
+}
+
+function plotGraph(fx) {
+  scene.remove(activeGraphs[0]);
+
+  activeGraphs.shift();
+
+  const graphPoints = [];
+
+  for (
+    let x = -xScale / 2 + camera.position.x;
+    x <= xScale / 2 + camera.position.x;
+    x += 1 / renderer.domElement.width
+  ) {
+    parser.set("x", x);
+    const point = { x: x, y: parser.evaluate(fx), z: 0 };
+    if (point.y) graphPoints.push(new THREE.Vector3(point.x, point.y, point.z));
+  }
+
+  const graphGeometry = new THREE.TubeGeometry(
+    new THREE.CatmullRomCurve3(graphPoints),
+    graphPoints.length,
+    0.05,
+    4,
+    false
+  );
+
+  const graphMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+
+  const graph = new THREE.Mesh(graphGeometry, graphMaterial);
+
+  activeGraphs.push(graph);
+
+  scene.add(graph);
+}
+
+controls.addEventListener("end", draw);
+
+function draw() {
+  xScale = visibleWidthAtZDepth(camera.position.z, camera) / 2; // doppelt so viel, wie man sehen kann
+  yScale = visibleHeightAtZDepth(camera.position.z, camera) / 2;
+
+  drawGrid();
+  if (activeFunctions[0]) plotGraph(activeFunctions[0]);
 }
